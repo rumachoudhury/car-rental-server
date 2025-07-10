269ab83d4623692919f96c30062c25d0a9a2ecdf
Ruma Choudhury <rumachoudhury7862@gmail.com>
2025-07-09 23:37:06 -0400

    Started backend prject

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
+
+//initialize Express App
+const app = express();
+
+//connect database
+// await connectDB();
+connectDB();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Server is running"));
+app.use("/api/user", userRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
